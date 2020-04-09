d3c75f2eb8f7d582520fcb985c7ed47aad6187b6
Debojyoti Chatterjee <debojyoti.chatterjee9@gmail.com>
2020-04-09 21:01:00 +0530

    Class Based Views - DetailView

--- source/blog/urls.py
+++ source/blog/urls.py
@@ -7,5 +7,9 @@ from .views import (
 app_name = 'blog'
 urlpatterns = [
     path('', ArticleListView.as_view(), name='article_list'),
-    
+
+    # by default django looks for a pk(primary key) or a slug as param
+    # path('<int:pk>/', ArticleDetailView.as_view(), name='article_detail')
+    # below is another way to do it
+    path('<int:id>/', ArticleDetailView.as_view(), name='article_detail')
 ]
--- source/blog/views.py
+++ source/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import (
     CreateView,
     DetailView,
@@ -11,3 +11,11 @@ from .models import Article
 class ArticleListView(ListView):
     template_name = 'articles/article_list.html'
     queryset = Article.objects.all() #<blog>/<modelname>/_<generic view name> -  this is how Django looks for a template for class based view function
+
+class ArticleDetailView(DetailView):
+    template_name = 'articles/article_detail.html'
+    # queryset = Article.objects.all()
+
+    def get_object(self):
+        id_ = self.kwargs.get("id")
+        return get_object_or_404(Article, id = id_)

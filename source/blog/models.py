027ba24635de4c060315ce18776bb016bd15f58a
Debojyoti Chatterjee <debojyoti.chatterjee9@gmail.com>
2020-04-09 20:02:11 +0530

    added blog app to the admin

--- source/blog/models.py
+++ source/blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Article(models.Model):
+    title        = models.CharField(max_length=120)  # max_length = required length
+    content      = models.TextField(blank=True, null=True)
+    active       = models.BooleanField(default=True)

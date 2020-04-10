a205545769b347e7b4f347da3f297a1b241a32c1
Debojyoti Chatterjee <debojyoti.chatterjee9@gmail.com>
2020-04-10 15:53:45 +0530

    Function Based View to Class Based View and added templates for job

--- source/job/views.py
+++ source/job/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.views import View
+
+# BASE VIEW CLASS = VIEW
+
+class JobView(View):
+    template_name = 'job/job_list.html'
+    def get(self, request, id=None, *args, **kwargs):
+        return render(request, self.template_name, {})
 
 # Create your views here.
 def myfbv(request, *args, **kwargs):
-    return render(request, 'about.html', {})
+    return render(request, 'contact.html', {})

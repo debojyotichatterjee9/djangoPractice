f57a4939816f8866c2c144f02e729598f4a1e240
Debojyoti Chatterjee <debojyoti.chatterjee9@gmail.com>
2020-04-10 14:58:13 +0530

    updated urls and view for job app

--- source/job/urls.py
+++ source/job/urls.py
@@ -1,20 +1,21 @@
 from django.urls import path
 from .views import (
-    JobView,
-    JobListView,
-    JobCreateView,
-    JobUpdateView,
-    JobDeleteView
-    # my_fbv
+    # JobView,
+    # JobListView,
+    # JobCreateView,
+    # JobUpdateView,
+    # JobDeleteView
+    myfbv
 )
 
 app_name = 'job'
 urlpatterns = [
-    path('', JobListView.as_view(), name='jobs_list'),
-    # path('', my_fbv, name='Jobs-list'),
+    # path('', JobListView.as_view(), name='jobs_list'),
+    path('', myfbv, name='jobs_list'),
 
 
-    path('create/', JobCreateView.as_view(), name='jobs_create'),
-    path('<int:id>/', JobView.as_view(), name='jobs_detail'),
-    path('<int:id>/update/', JobUpdateView.as_view(), name='jobs_update'),
-    path('<int:id>/delete/', JobDeleteView.as_view(), name='jobs_delete'),
+    # path('create/', JobCreateView.as_view(), name='jobs_create'),
+    # path('<int:id>/', JobView.as_view(), name='jobs_detail'),
+    # path('<int:id>/update/', JobUpdateView.as_view(), name='jobs_update'),
+    # path('<int:id>/delete/', JobDeleteView.as_view(), name='jobs_delete'),
+]
--- source/job/views.py
+++ source/job/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def myfbv(request, *args, **kwargs):
+    return render(request, 'about.html', {})

ddf182f69684dfe2e93d85f1547d6984aad4647d
Debojyoti Chatterjee <debojyoti.chatterjee9@gmail.com>
2020-09-12 17:22:21 +0530

    added product model and basic ops via admin and python shell

--- src/products/admin.py
+++ src/products/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+# COMMENT: This is called a relative import
+from .models import Product
+
 # Register your models here.
+admin.site.register(Product)
--- src/products/models.py
+++ src/products/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 # Create your models here.
 # COMMENT: Inherit this from the default Django class of model 
-class Products(models.Model):
+class Product(models.Model):
     title       = models.TextField()
     description = models.TextField()
     price       = models.TextField()
+    summary     = models.TextField(default='This is a sample sumary.')

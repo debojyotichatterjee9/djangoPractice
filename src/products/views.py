2e801156d3519b43f13477cf457d6fb699c07550
Debojyoti Chatterjee <debojyoti.chatterjee9@gmail.com>
2020-10-04 23:06:01 +0530

    updating the context and updating if clause

--- src/products/views.py
+++ src/products/views.py
@@ -4,11 +4,15 @@ from .models import Product
 
 def product_detail_view(request):
     obj = Product.objects.get(id=1)
+    # context = {
+    #     "title": obj.title,
+    #     "description": obj.description,
+    #     "price": obj.price,
+    #     "summary": obj.summary,
+    #     "available": obj.available
+    # }
+#     For better usability changing the context and mapping to the product object
     context = {
-        "title": obj.title,
-        "description": obj.description,
-        "price": obj.price,
-        "summary": obj.summary,
-        "available": obj.available
+        "product": obj
     }
     return render(request, "product/product_detail.html", context)

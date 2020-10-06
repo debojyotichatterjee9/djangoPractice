92d8f1985a541347f6f435e246076d08adb8fe24
Debojyoti Chatterjee <debojyoti.chatterjee9@gmail.com>
2020-10-06 19:48:00 +0530

    pure django form and form widgets

--- src/products/forms.py
+++ src/products/forms.py
@@ -14,8 +14,29 @@ class ProductForm(forms.ModelForm):
         
         
 class RawProductForm(forms.Form):
-    title       = forms.CharField()
-    description = forms.CharField()
-    summary     = forms.CharField()
-    price       = forms.DecimalField()
-    available   = forms.BooleanField()
+    title       = forms.CharField(widget=forms.TextInput(
+        attrs={
+            "placeholder": "Enter the product name...",
+            }))
+    description = forms.CharField(
+        required=False,
+        widget=forms.Textarea(
+            attrs={
+                "id": "product-desc",
+                "placeholder": "Enter the product description...",
+                "class": "style-class-name1 style-class-name2",
+                "rows": 9,
+                "columns": 70
+                }))
+    summary     = forms.CharField(
+        required=False,
+        widget=forms.Textarea(
+            attrs={
+                "id": "product-summary",
+                "placeholder": "Enter the product summary...",
+                "class": "style-class-name1 style-class-name2",
+                "rows": 9,
+                "columns": 70
+                }))
+    price       = forms.DecimalField(initial=199.99)
+    available   = forms.BooleanField(initial=True)

c27a6996a833e6a04b38b946f9820a1b97ff520b
Debojyoti Chatterjee <debojyoti.chatterjee9@gmail.com>
2020-10-13 01:36:37 +0530

    class based view for user_list

--- src/users/urls.py
+++ src/users/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import user_list_view, user_detail_view
+from .views import user_detail_view
+from .views import (
+    UserListView
+)
 
 app_name="users"
 urlpatterns = [
-    path('list', user_list_view, name='list'),
+    # path('list', user_list_view, name='list'),
+    path('list', UserListView.as_view(), name='list'),
     path('details/<str:user_id>', user_detail_view, name='details'),
     ]
--- src/users/views.py
+++ src/users/views.py
@@ -1,16 +1,29 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import (
+    CreateView,
+    DetailView,
+    ListView,
+    UpdateView,
+    DeleteView,
+)
 from django.http import Http404
 from .models import User
 from .forms import UserForm
 
 
 ''' ==================================== USER LIST VIEW ==================================== '''
-def user_list_view(request):
+# def user_list_view(request):
+#     queryset = User.objects.all()
+#     context = {
+#         "user_list": queryset
+#     }
+#     return render(request, "users/users_list.html", context)
+
+class UserListView(ListView):
+    # by default django class based view will search this loaction -> <users>/<model_name>_list.html
+    # but we can provide our own template in custom path
+    template_name = "users/users_list.html"
     queryset = User.objects.all()
-    context = {
-        "user_list": queryset
-    }
-    return render(request, "users/users_list.html", context)
 
 
 ''' ==================================== USER DETAIL VIEW ==================================== '''
